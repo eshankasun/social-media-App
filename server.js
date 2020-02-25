6e75d38ac8dab5b8003a8bf4039a1707f33662c0
eshankasun <54253735+eshankasun@users.noreply.github.com>
2020-02-25 19:25:07 +0530

    first Commit
    Mern Stack
    
    socail media platform for devs.

--- server.js
+++ server.js
@@ -1,88 +1,138 @@
-const mongoose = require('mongoose')
-const express = require('express')
+/** @format */
+
+const express = require('express');
 const app = express();
-const methodOverride = require('method-override')
-const flash = require("express-flash");
-const session = require("express-session");
-const User = require('./model/User')
+const methodOverride = require('method-override');
+const flash = require('express-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const verifyToken = require('./verifyToken');
+const generateToken = require('./generateToken');
+const JWT = require('jsonwebtoken');
+const connectDB = require('./config/db');
 
+const dotenv = require('dotenv');
+const PORT = process.env.PORT || 5000;
 
-
-
-
-const dotenv = require('dotenv')
-const PORT = process.env.PORT || 3000
-
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
 app.use(flash());
 app.use(
-    session({
-        secret: "anything",
-        resave: false,
-        saveUninitialized: false
-    })
+  session({
+    secret: 'anything',
+    resave: false,
+    saveUninitialized: false
+  })
 );
+app.use(bodyParser());
 
+app.use(express.json());
+app.use(cors());
 
+// // set a cookie
+// app.use('/', (req, res, next) => {
+//     // check if client sent cookie
+//     var cookie = req.cookies.cookieName;
+//     if (cookie === undefined) {
+//         // no: set a new cookie
+//         var randomNumber = Math.random().toString();
+//         randomNumber = randomNumber.substring(2, randomNumber.length);
+//         res.cookie('cookieName', randomNumber, { maxAge: 900000, httpOnly: true });
+//         console.log('cookie created successfully');
+//     }
+//     else {
+//         // yes, cookie was already present
+//         console.log('cookie exists', cookie);
+//     }
+//     next(); // <-- important!
+// });
 
 //get home page
+//get home page
 
-app.get('/', (req, res) => {
-    res.render('index.ejs', { name: 'kyle' })
-})
+// define Routes
+// app.use('/api/users',require('./routes/api/users'))
+// app.use('/api/auth',require('./routes/api/auth'))
+// app.use('/api/profile',require('./routes/api/profile'))
+// app.use('/api/posts',require('./routes/api/posts'))
+
+app.get('/', verifyToken, (req, res) => {
+  res.render('index.ejs', { name: req.body.User });
+});
 //get login page
 app.get('/login', (req, res) => {
-    res.render('login.ejs')
-})
+  res.render('login.ejs');
+});
 //get register page
 app.get('/register', (req, res) => {
-    res.render('register.ejs')
-})
+  res.render('register.ejs');
+});
 
-//post
-app.post('/register', (req, res) => {
+app.get('/', verifyToken, (req, res) => {
+  // User.findOne({ id: User }, 'name email token', (err, docs) => {
 
-})
-app.post('/login', (req, res) => {
+  //     res.render('index.ejs', {
+  //         user: docs,
 
-})
+  //     })
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies);
 
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies);
+});
 
 // import Routes
-const authRoute = require('./auth')
-const postRoute = require('./routes/post')
+// const authRoute = require('./auth');
+const postRoute = require('./routes/api/post');
+const userRoute = require('./routes/api/users');
+const authRoute = require('./routes/api/auth');
+const profileRoute = require('./routes/api/profile');
 // dot env init
-dotenv.config()
+dotenv.config();
 
-//connect to DB
-mongoose.connect(process.env.DB_CONNECT,
-    { useUnifiedTopology: true }, () => console.log('Connected to DB!'))
+// //connect to DB
+connectDB();
+// mongoose.connect(process.env.DB_CONNECT,
+//     { useUnifiedTopology: true }, () => console.log('Connected to DB!'))
+
+// var User = require('./model/User');
 
 //Middleware
-app.use(express.json());
+app.use(express.json({ extended: false }));
 
-app.get('/', function (req, res) {
-    req.flash('info', 'Welcome');
-    res.render('index', {
-        title: 'Home'
-    })
+app.get('/', function(req, res) {
+  req.flash('info', 'Welcome');
+  res.render('index', {
+    title: 'Home'
+  });
 });
-app.get('/addFlash', function (req, res) {
-    req.flash('info', 'Flash Message Added');
-    res.redirect('/');
+app.get('/addFlash', function(req, res) {
+  req.flash('info', 'Flash Message Added');
+  res.redirect('/');
 });
-
 
 //route Middlewares
-app.use('/api/user', authRoute)
+// app.use('/api/user', authRoute);
 
-app.use('/api/post', postRoute)
+app.use('/api/post', postRoute);
 
+app.use('/api/users', userRoute);
 
+app.use('/api/auth', authRoute);
 
+app.use('/api/profile', profileRoute);
 
+// // console.log(User.find())
+// User.findOne().exec(function (err, docs) {
+//     console.log(docs.name)
+//     userId = docs.name
+//     console.log(userId)
+// });
 
-app.listen(3000, () => console.log(`Server started on Port ${PORT}`))
+app.listen(5000, () => console.log(`Server started on Port ${PORT}`));
